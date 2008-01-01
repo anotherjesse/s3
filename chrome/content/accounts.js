a99db0ac6ce8f9ba92a3c033beb981ad15ec29e1
joshua <joshua@Macintosh-3.local>
2007-12-31 22:22:00 -0800

    Got accounts page working

--- chrome/content/accounts.js
+++ chrome/content/accounts.js
@@ -8,4 +8,84 @@
 // for the specific language governing rights and limitations under the
 // License.
 
+	
+	var $ = function(x) { return document.getElementById(x) };
+	var CC = Components.classes;
+	var CI = Components.interfaces;
+  var htmlNS = "http://www.w3.org/1999/xhtml";
+	
+	function s3Control() {
+		var inst = this;
 
+		const PREFS = CC['@mozilla.org/preferences-service;1'].getService(CI.nsIPrefService).getBranch('extension.s3.');
+		
+		this.save = function() {
+	    PREFS.setCharPref('key', $('s3-key').value);
+	    PREFS.setCharPref('secret_key', $('s3-secret-key').value);
+	    // inst.load();
+	  }
+
+	  this.load = function() {
+	    S3Ajax.KEY_ID = PREFS.getCharPref('key');
+	    S3Ajax.SECRET_KEY = PREFS.getCharPref('secret_key');
+	    inst.list();
+	  }
+
+	  this.setup = function() {
+	    if (PREFS.getPrefType('key')) {
+	      $('s3-key').value = PREFS.getCharPref('key');
+	    }
+	    if (PREFS.getPrefType('secret_key')) {
+	      $('s3-secret-key').value = PREFS.getCharPref('secret_key');
+	      inst.load();
+	    }
+      
+	  }
+	  
+	  this.addDir = function( dirname ) {
+      var tr=document.createElementNS(htmlNS, 'tr');
+      tr.setAttribute('id', dirname);
+
+      var td=document.createElementNS(htmlNS, 'td');
+      var a=document.createElementNS(htmlNS, 'a');
+      a.appendChild( document.createTextNode( dirname ));
+      a.setAttribute("href", "s3://" + dirname);
+      td.appendChild( a );
+      tr.appendChild( td );
+
+      $('keys').appendChild( tr );
+    }
+	
+    this.list = function() {
+      while ($('files').hasChildNodes()) 
+        $('files').removeChild( $('files').lastChild );
+
+      var table = document.createElementNS(htmlNS, 'table');
+      table.setAttribute('id', 'keys');
+      var tr = document.createElementNS(htmlNS, 'tr');
+      var th = document.createElementNS(htmlNS, 'th');
+      th.appendChild( document.createTextNode('Key') );
+      tr.appendChild( th );
+      var th = document.createElementNS(htmlNS, 'th');
+      th.appendChild( document.createTextNode('Last Modified') );
+      tr.appendChild( th );
+      var th = document.createElementNS(htmlNS, 'th');
+      th.appendChild( document.createTextNode('Size') );
+      tr.appendChild( th );
+      var th = document.createElementNS(htmlNS, 'th');
+      th.appendChild( document.createTextNode('Actions') );
+      tr.appendChild( th );
+      
+      table.appendChild(tr);
+      $('files').appendChild( table );
+
+      S3Ajax.listBuckets(
+        function(xml, objs) {
+          var buckets = objs.ListAllMyBucketsResult.Buckets.Bucket;
+          for (var i=0; i<buckets.length; i++) {
+            inst.addDir(buckets[i].Name);
+          }
+        });
+    }
+	}
+	
