b04fcac2a11e2540843facae0701bad138d615ef
Jesse Andrews <anotherjesse@gmail.com>
2008-02-20 21:38:54 -0800

    drag and drop works again ;)

--- chrome/content/dnd.js
+++ chrome/content/dnd.js
@@ -1,15 +1,14 @@
 // Copyright Jesse Andrews, 2005-2008
 // http://overstimulate.com
-// 
+//
 // This file may be used under the terms of of the
 // GNU General Public License Version 2 or later (the "GPL"),
 // http://www.gnu.org/licenses/gpl.html
-// 
+//
 // Software distributed under the License is distributed on an "AS IS" basis,
 // WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 // for the specific language governing rights and limitations under the
 // License.
-
 var dnd = {
   onDragOver: function(aEvent, aFlavor, aSession) {},
   onDragEnter: function(aEvent, aSession) {},
@@ -18,14 +17,15 @@ var dnd = {
   canHandleMultipleItems: true,
 
   onDrop: function(aEvent, aDropData, aSession) {
+    aEvent.stopPropagation();
     for (var c = 0; c < aDropData.dataList.length; c++) {
       var supports = aDropData.dataList[c].dataList[0].supports;
       var contentType = aDropData.dataList[c].dataList[0].flavour.contentType;
-            
+
       switch (contentType) {
         case "application/x-moz-file":
           var file = Components.classes["@mozilla.org/file/local;1"]
-            .createInstance(Componens.interfaces..nsILocalFile);
+            .createInstance(Components.interfaces.nsILocalFile);
           file.initWithPath( aDropData.dataList[c].dataList[0].data.path );
           alert('upload: ' + file);
           break;
@@ -47,7 +47,7 @@ var dnd = {
       }
     }
   },
-  
+
   getSupportedFlavours: function() {
     var flavors = new FlavourSet();
     flavors.appendFlavour("application/x-moz-file", "nsIFile");
