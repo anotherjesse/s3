6b27cb03fbe07c7ef1babc8bc7461eb3ff17b4ab
Jesse Andrews <anotherjesse@gmail.com>
2008-02-21 03:00:36 -0800

    spacing fixes...  and trim the values you set

--- chrome/content/accounts.js
+++ chrome/content/accounts.js
@@ -10,85 +10,87 @@
 // for the specific language governing rights and limitations under the
 // License.
 
-  var $ = function(x) { return document.getElementById(x) };
-  var CC = Components.classes;
-  var CI = Components.interfaces;
+var $ = function(x) { return document.getElementById(x) };
+var CC = Components.classes;
+var CI = Components.interfaces;
 
-  function s3Control() {
-    var inst = this;
+function s3Control() {
+  var inst = this;
 
-    const PREFS = CC['@mozilla.org/preferences-service;1'].getService(CI.nsIPrefService).getBranch('extension.s3.');
+  const PREFS = CC['@mozilla.org/preferences-service;1'].getService(CI.nsIPrefService).getBranch('extension.s3.');
 
-    function set(key, val) {
-      try {
-        if (val && val.length > 0) {
-          PREFS.setCharPref(key, val)
-        }
-        else {
-          PREFS.clearUserPref(key)
-        }
+  function set(key, val) {
+    try {
+      if (val) {
+        val = val.replace(/^\s+|\s+$/g, '');
       }
-      catch (e) {}
-    }
-
-    this.save = function() {
-      set('key', $('s3-key').value);
-      set('secret_key', $('s3-secret-key').value);
-      inst.load();
-      $('account').style.display = 'none';
-    }
-
-    this.load = function() {
-      try {
-        S3Ajax.KEY_ID = PREFS.getCharPref('key');
-        S3Ajax.SECRET_KEY = PREFS.getCharPref('secret_key');
-        inst.list();
+      if (val && val.length > 0) {
+        PREFS.setCharPref(key, val)
       }
-      catch(e) {};
-    }
-
-    this.setup = function() {
-      if (PREFS.getPrefType('key')) {
-        $('s3-key').value = PREFS.getCharPref('key');
+      else {
+        PREFS.clearUserPref(key)
       }
-      if (PREFS.getPrefType('secret_key')) {
-        $('s3-secret-key').value = PREFS.getCharPref('secret_key');
-        if ($('s3-secret-key').value != '') {
-          inst.load();
-        }
-        else {
-          setkeys();
-        }
+    }
+    catch (e) {}
+  }
+
+  this.save = function() {
+    set('key', $('s3-key').value);
+    set('secret_key', $('s3-secret-key').value);
+    inst.load();
+    $('account').style.display = 'none';
+  }
+
+  this.load = function() {
+    try {
+      S3Ajax.KEY_ID = PREFS.getCharPref('key');
+      S3Ajax.SECRET_KEY = PREFS.getCharPref('secret_key');
+      inst.list();
+    }
+    catch(e) {};
+  }
+
+  this.setup = function() {
+    if (PREFS.getPrefType('key')) {
+      $('s3-key').value = PREFS.getCharPref('key');
+    }
+    if (PREFS.getPrefType('secret_key')) {
+      $('s3-secret-key').value = PREFS.getCharPref('secret_key');
+      if ($('s3-secret-key').value != '') {
+        inst.load();
       }
       else {
         setkeys();
       }
     }
-
-    this.addDir = function( dirname ) {
-      var tr=document.createElement('tr');
-      tr.setAttribute('id', dirname);
-
-      var td=document.createElement('td');
-      var a=document.createElement('a');
-      a.appendChild( document.createTextNode( dirname ));
-      a.setAttribute("href", "s3://" + dirname);
-      td.appendChild( a );
-      tr.appendChild( td );
-
-      $('buckets').appendChild( tr );
-    }
-
-    this.list = function() {
-      $('buckets').innerHTML = '';
-
-      S3Ajax.listBuckets(
-        function(xml, objs) {
-          var buckets = xml.responseXML.getElementsByTagName('Bucket');
-          for (var i=0; i<buckets.length; i++) {
-            inst.addDir(buckets[i].getElementsByTagName('Name')[0].textContent);
-          }
-        });
+    else {
+      setkeys();
     }
   }
 
+  this.addDir = function( dirname ) {
+    var tr=document.createElement('tr');
+    tr.setAttribute('id', dirname);
+
+    var td=document.createElement('td');
+    var a=document.createElement('a');
+    a.appendChild( document.createTextNode( dirname ));
+    a.setAttribute("href", "s3://" + dirname);
+    td.appendChild( a );
+    tr.appendChild( td );
+
+    $('buckets').appendChild( tr );
+  }
+
+  this.list = function() {
+    $('buckets').innerHTML = '';
+
+    S3Ajax.listBuckets(
+      function(xml, objs) {
+        var buckets = xml.responseXML.getElementsByTagName('Bucket');
+        for (var i=0; i<buckets.length; i++) {
+          inst.addDir(buckets[i].getElementsByTagName('Name')[0].textContent);
+        }
+      });
+  }
+}
