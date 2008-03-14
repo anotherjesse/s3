28e2dc53f4e0742d829c18c74802ba393d2c8bd4
Jesse Andrews <anotherjesse@gmail.com>
2008-03-14 00:46:47 -0700

    more jquery-ization the account listing

--- chrome/content/accounts.js
+++ chrome/content/accounts.js
@@ -10,96 +10,85 @@
 // for the specific language governing rights and limitations under the
 // License.
 
-function s3Control() {
-  var inst = this;
-  var $ = function(x) { return document.getElementById(x) };
 
+function setkeys() {
+  $('#account').show();
+}
+
+function s3Control() {
   const PREFS = Components.classes['@mozilla.org/preferences-service;1']
     .getService(Components.interfaces.nsIPrefService).getBranch('extension.s3.');
 
-  function set(key, val) {
-    try {
-      if (val) {
-        val = val.replace(/^\s+|\s+$/g, '');
-      }
-      if (val && val.length > 0) {
-        PREFS.setCharPref(key, val)
-      }
-      else {
-        PREFS.clearUserPref(key)
-      }
-    }
-    catch (e) {}
-  }
-
-  this.save = function() {
-    set('key', $('s3-key').value);
-    set('secret_key', $('s3-secret-key').value);
-    inst.load();
-    $('account').style.display = 'none';
-  }
-
-  this.load = function() {
-    try {
-      S3Ajax.KEY_ID = PREFS.getCharPref('key');
-      S3Ajax.SECRET_KEY = PREFS.getCharPref('secret_key');
-      inst.list();
-    }
-    catch(e) {};
-  }
-
-  this.setup = function() {
-    if (PREFS.getPrefType('key')) {
-      $('s3-key').value = PREFS.getCharPref('key');
-    }
-    if (PREFS.getPrefType('secret_key')) {
-      $('s3-secret-key').value = PREFS.getCharPref('secret_key');
-      if ($('s3-secret-key').value != '') {
-        inst.load();
-      }
-      else {
-        setkeys();
-      }
-    }
-    else {
-      setkeys();
-    }
-  }
-
-  this.addDir = function( dirname ) {
+  function addBucket( bucket ) {
     var tr=document.createElement('tr');
-    tr.setAttribute('id', dirname);
+    tr.setAttribute('id', bucket);
 
     var td=document.createElement('td');
     var a=document.createElement('a');
-    a.appendChild( document.createTextNode( dirname ));
-    a.setAttribute("href", "s3://" + dirname);
+    a.appendChild( document.createTextNode(bucket) );
+    a.setAttribute("href", "s3://" + bucket);
     td.appendChild( a );
     tr.appendChild( td );
 
-    $('buckets').appendChild( tr );
+    document.getElementById('buckets').appendChild(tr);
   }
 
-  this.list = function() {
-    $('buckets').innerHTML = '';
+  function list() {
+    $('#buckets').empty();
 
     S3Ajax.listBuckets(
       function(xml, objs) {
         var buckets = xml.responseXML.getElementsByTagName('Bucket');
         for (var i=0; i<buckets.length; i++) {
-          inst.addDir(buckets[i].getElementsByTagName('Name')[0].textContent);
-          $('active').style.display = '';
+          addBucket(buckets[i].getElementsByTagName('Name')[0].textContent);
         }
+        $('#active').show();
       });
   }
+
+  function load() {
+    try {
+      S3Ajax.KEY_ID = PREFS.getCharPref('key');
+      S3Ajax.SECRET_KEY = PREFS.getCharPref('secret_key');
+      list();
+    }
+    catch(e) {};
+  }
+    
+  this.save = function() {
+    function set_pref(key, val) {
+      try {
+        if (val) {
+          val = val.replace(/^\s+|\s+$/g, '');
+        }
+        if (val && val.length > 0) {
+          PREFS.setCharPref(key, val)
+        }
+        else {
+          PREFS.clearUserPref(key)
+        }
+      }
+      catch (e) {}
+    }
+    
+    set_pref('key', $('#s3-key').val());
+    set_pref('secret_key', $('#s3-secret-key').val());
+    load();
+    $('#account').hide();
+  }
+  
+  if (PREFS.getPrefType('key')) {
+    $('#s3-key').val(PREFS.getCharPref('key'));
+  }
+
+  if (PREFS.getPrefType('secret_key')) {
+    $('#s3-secret-key').val(PREFS.getCharPref('secret_key'));
+  }
+
+  load();
 }
 
-function setkeys() {
-  document.getElementById('account').style.display='block';
-}
-
-var s3_controller = new s3Control();
-window.onload = function() {
-  s3_controller.setup();
-}
+$(function() {
+  s3_controller = new s3Control();
+});
 
