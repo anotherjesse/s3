7099d61b6b0919fe954b522a3e74cda3c506ce64
Jesse Andrews <anotherjesse@gmail.com>
2008-06-29 22:58:12 -0700

    include modules in the xpi

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,5 +2,5 @@ task :default => [:xpi]
 
 task :xpi do
   rm_f 's3.xpi'
-  `find chrome chrome.manifest components install.rdf | egrep -v "(~|#)" | xargs zip s3.xpi`
+  `find chrome modules chrome.manifest components install.rdf | egrep -v "(~|#)" | xargs zip s3.xpi`
 end 
